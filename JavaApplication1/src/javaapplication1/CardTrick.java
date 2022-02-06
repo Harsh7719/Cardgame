e615d940998f40e6b4b51abb0c1f01081ae3eb3c
Harsh7719 <98338705+Harsh7719@users.noreply.github.com>
2022-02-07 01:51:39 +0530

    third commit

--- JavaApplication1/src/javaapplication1/CardTrick.java
+++ JavaApplication1/src/javaapplication1/CardTrick.java
@@ -1,12 +1,10 @@
+// i am done 
 //Harshdeep varma date:5 feburary 2002
 
 package javaapplication1;
 public class CardTrick {
     // harshdeep varma 
-    System.out.println(" this is harsh");
-    
-    
-    
+    System.out.println("this is harsh");
     
     
 }
